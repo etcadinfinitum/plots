4bebc8e76124d1abe1e9c29452cce23823fa2237
Lizzy <lizzy.presland@gmail.com>
2018-05-24 16:28:18 -0700

    Refactored pre-processing of numpy arrays, programmatically graphed bubble chart

--- Climbing/Plot.py
+++ Climbing/Plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as pyp
 import matplotlib.dates as mpldates
 import datetime
 import math
+import pdb
 
 # HELPER METHODS AND CONSTANTS
 
@@ -21,37 +22,18 @@ route_difficulty = { "Gray" : [0, "V0"],
                      "Purple": [6, "V6"],
                      "Black": [7, "V7"] }
 
-def get_level_number(color):
-    return route_difficulty.get(color)[0]
-
-def get_level_name(color):
-    return route_difficulty.get(color)[1]
-
 # GET DATA, REFORMAT AS NEEDED
 
+# LAMBDAS
 datefunc = lambda x: mpldates.date2num(datetime.date(int(x[6:]), int(x[0:2]), int(x[3:5])))
+boolfunc = lambda val: True if conversion_constants.get(val.decode()) == True else False
 
+# LOADTXT
 activity, route_level, features, injury_type = np.loadtxt("./routes.csv", delimiter=",", skiprows=1, unpack=True, usecols=(1,2,5,7), dtype='str')
-
-pre_completion, pre_injury, pre_new_route, pre_overhang, pre_handicap = np.loadtxt("./routes.csv", delimiter=",", skiprows=1, unpack=True, usecols=(4,6,8,9,10), dtype='str')
-
-def convert_boolean_arrays(pre_array):
-    inter_array = []
-    for val in pre_array:
-        if val != 'nan':
-            inter_array.append(conversion_constants.get(val))
-        else:
-            inter_array.append(False)
-    return inter_array
-
-completion = np.array(convert_boolean_arrays(pre_completion))
-injury = np.array(convert_boolean_arrays(pre_injury))
-new_route = np.array(convert_boolean_arrays(pre_new_route))
-overhang = np.array(convert_boolean_arrays(pre_overhang))
-handicap = np.array(convert_boolean_arrays(pre_handicap))
-
+completion, injury, new_route, overhang, handicap = np.loadtxt("./routes.csv", delimiter=",", skiprows=1, unpack=True, usecols=(4,6,8,9,10), dtype='bool', converters={4: boolfunc, 6: boolfunc, 8: boolfunc, 9: boolfunc, 10: boolfunc})
 dates, quantity_time = np.loadtxt("./routes.csv", delimiter=",", skiprows=1, unpack=True, usecols=(0, 3), converters= {0: datefunc})
 
+# Function to find total completed/uncompleted per route color for  each distinct day
 def summarize_route_completion_by_date(fin_or_fail):
     new_dict = {}
     for i in range(len(dates)):
@@ -73,70 +55,46 @@ unfinished_routes = summarize_route_completion_by_date(False)
 print("\nFinished routes (full dict): " + str(finished_routes))
 print("Failed routes (full dict): " + str(unfinished_routes) + "\n")
 
-route_dates = []
-unfinished_routes_intermediate = {'Gray': [], 'Yellow': [], 'Green': [], 'Red': [], 'Blue': [], 'Orange': [], 'Purple': [], 'Black': [] }
-finished_routes_intermediate = {'Gray': [], 'Yellow': [], 'Green': [], 'Red': [], 'Blue': [], 'Orange': [], 'Purple': [], 'Black': [] }
+# get conversion dictionary to convert matplotlib dates to an ordered set of dates (1, 2, 3, ... N)
+def get_ordered_dates_dict():
+    date_list = dates.tolist()
+    new_dict = {}
+    counter = 1
+    for i in sorted(date_list):
+        if not i in new_dict:
+            new_dict.update({i: counter})
+            counter += 1
+    return new_dict
 
-
-# prep dictionaries for concise plotting?
-def convert_route_completion_summary():
-    for key in sorted(unfinished_routes):
-        if not key in route_dates:
-            route_dates.append(key)
-        for color in route_difficulty:
-            if unfinished_routes.get(key).get(color, None) == None:
-                unfinished_routes_intermediate.get(color).append(0)
-            else:
-                unfinished_routes_intermediate.get(color).append(unfinished_routes.get(key).get(color))
-    for key in sorted(finished_routes):
-        if not key in route_dates:
-            route_dates.append(key)
-        for color in route_difficulty:
-            if finished_routes.get(key).get(color, None) == None:
-                finished_routes_intermediate.get(color).append(0)
-            else:
-                finished_routes_intermediate.get(color).append(finished_routes.get(key).get(color))
-
-convert_route_completion_summary()
-
-# set up ordered (1, 2, 3, ... N) set of dates 
-route_dates_array = np.array(route_dates)
-route_dates_ordered = []
-for i in route_dates:
-    route_dates_ordered.append(route_dates.index(i) + 1)
-route_dates_ordered_array = np.array(route_dates_ordered)
+ordered_dates_dict = get_ordered_dates_dict()
 
 # set up "calendar" representation of route dates (a la github heatmap)
 route_dates_weekday = []
 route_dates_week = []
-for i in route_dates:
+for i in ordered_dates_dict:
     route_dates_weekday.append(mpldates.num2date(i).isoweekday())
-    route_dates_week.append(((int(i) - int(route_dates[0])) / 7) + 1)
+    route_dates_week.append(math.floor((int(i) - np.amin(dates)) / 7) + 1)
 print("Weekday series: " + str(route_dates_weekday))
 print("Week numbers: " + str(route_dates_week))
 
-finished_gray = np.array(finished_routes_intermediate.get('Gray'))
-finished_yellow = np.array(finished_routes_intermediate.get('Yellow'))
-finished_green = np.array(finished_routes_intermediate.get('Green'))
-finished_red = np.array(finished_routes_intermediate.get('Red'))
-finished_blue = np.array(finished_routes_intermediate.get('Blue'))
-finished_orange = np.array(finished_routes_intermediate.get('Orange'))
-finished_purple = np.array(finished_routes_intermediate.get('Purple'))
-finished_black = np.array(finished_routes_intermediate.get('Black'))
-failed_gray = np.array(unfinished_routes_intermediate.get('Gray'))
-failed_yellow = np.array(unfinished_routes_intermediate.get('Yellow'))
-failed_green = np.array(unfinished_routes_intermediate.get('Green'))
-failed_red = np.array(unfinished_routes_intermediate.get('Red'))
-failed_blue = np.array(unfinished_routes_intermediate.get('Blue'))
-failed_orange = np.array(unfinished_routes_intermediate.get('Orange'))
-failed_purple = np.array(unfinished_routes_intermediate.get('Purple'))
-failed_black = np.array(unfinished_routes_intermediate.get('Black'))
+bubble_icon_size_multiplier = 15
 
-# helpful printouts (just to confirm data is being processed properly)
-print("route dates array: " + str(route_dates_array))
-print("finished_gray data: " + str(finished_gray))
-print("finished_yellow data: " + str(finished_yellow))
-print("finished_green data: " + str(finished_green))
+def get_bubble_arrays(working_dict):
+    bubble_dates = []
+    bubble_color = []
+    bubble_count = []
+    bubble_diff_no = []
+    for date in working_dict:
+        for color in route_difficulty:
+            if color in working_dict[date]:
+                bubble_dates.append(ordered_dates_dict.get(date))
+                bubble_color.append(color.lower())
+                bubble_diff_no.append(route_difficulty.get(color)[0] + 0.5)
+                bubble_count.append(bubble_icon_size_multiplier * working_dict[date][color])
+    return (bubble_dates, bubble_color, bubble_count, bubble_diff_no)
+
+finished_bubbles = get_bubble_arrays(finished_routes)
+unfinished_bubbles = get_bubble_arrays(unfinished_routes)
 
 # plotting 
 
@@ -176,67 +134,40 @@ weight_profile_weight = weight_profile.twinx()
 weight_profile_weight.set_ylim(0, np.amax(weight))
 weight = weight_profile_weight.plot(date, weight, color='red', ls='-', marker='o', label='Body Weight (lbs)')
 weight_profile_weight.set(ylabel='Weight (lbs)')
-weight_handles.append(bf[0])
-weight_handles.append(h20[0])
-weight_handles.append(bone[0])
-weight_handles.append(weight[0])
-weight_handles.append(musc[0])
+weight_handles += [bf[0], h20[0], bone[0], musc[0]]
 
 # plot bubble chart
 
 bubble_handles = []
 
-summary_bubble_plot.set_xlim(min(route_dates_ordered_array) - 1, max(route_dates_ordered_array) + 1)
+summary_bubble_plot.set_xlim(min(ordered_dates_dict.values()) - 1, max(ordered_dates_dict.values()) + 1)
 summary_bubble_plot.set_ylim(0, 8)
 summary_bubble_plot.set_title('Route Completion vs Failure by Difficulty')
 summary_bubble_plot.set(xlabel='Session No.', ylabel='Route Difficulty')
 
-# plot failed routes for bubble chart
-fails = summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 0.5, c='gray', marker='s', s=failed_gray*15, alpha=0.5, label="Uncompleted Attempts")
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 1.5, c='yellow', marker='s', s=failed_yellow*15, alpha=0.5)
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 2.5, c='green', marker='s', s=failed_green*15, alpha=0.5)
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 3.5, c='red', marker='s', s=failed_red*15, alpha=0.5)
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 4.5, c='blue', marker='s', s=failed_blue*15, alpha=0.5)
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 5.5, c='orange', marker='s', s=failed_orange*15, alpha=0.5)
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 6.5, c='purple', marker='s', s=failed_purple*15, alpha=0.5)
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 7.5, c='black', marker='s', s=failed_black*15, alpha=0.5)
-
-
-# plot finished routes for bubble chart
-fins = summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 0.5, c='gray', marker='o', s=finished_gray*15, edgecolors='black', label="Completed Attempts")
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 1.5, c='yellow', marker='o', s=finished_yellow*15, edgecolors='black')
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 2.5, c='green', marker='o', s=finished_green*15, edgecolors='black')
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 3.5, c='red', marker='o', s=finished_red*15, edgecolors='black')
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 4.5, c='blue', marker='o', s=finished_blue*15, edgecolors='black')
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 5.5, c='orange', marker='o', s=finished_orange*15, edgecolors='black')
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 6.5, c='purple', marker='o', s=finished_purple*15, edgecolors='black')
-summary_bubble_plot.scatter(route_dates_ordered_array, route_dates_array - route_dates_array + 7.5, c='black', marker='o', s=finished_black*15, edgecolors='black')
-
-bubble_handles.append(fails)
-bubble_handles.append(fins)
+summary_bubble_plot.scatter(unfinished_bubbles[0], unfinished_bubbles[3], c=unfinished_bubbles[1], s=unfinished_bubbles[2], alpha=0.5, marker='s')
+summary_bubble_plot.scatter(finished_bubbles[0], finished_bubbles[3], c=finished_bubbles[1], s=finished_bubbles[2], marker='o', edgecolors='black')
 
 # plot relative frequencies of success/fail per day
-total_finished_per_day = []
-total_unfinished_per_day = []
-for i in route_dates_ordered:
-    idx = route_dates_ordered.index(i)
-    total_finished_per_day.append(finished_gray[idx] + finished_yellow[idx] + finished_green[idx] + finished_red[idx] + finished_blue[idx] + finished_orange[idx] + finished_purple[idx] + finished_black[idx])
-    total_unfinished_per_day.append(failed_gray[idx] + failed_yellow[idx] + failed_green[idx] + failed_red[idx] + failed_blue[idx] + failed_orange[idx] + failed_purple[idx] + failed_black[idx])
-
+other_handles = []
 total_finished_ratio = []
 total_unfinished_ratio = []
-for i in route_dates_ordered:
-    idx = route_dates_ordered.index(i)
-    total = float(total_finished_per_day[idx] + total_unfinished_per_day[idx])
-    total_finished_ratio.append(100 * float(total_finished_per_day[idx]) / total)
-    total_unfinished_ratio.append(100 * float(total_unfinished_per_day[idx]) / total)
+total_ratio_dates = []
+for date in ordered_dates_dict:
+    total_ratio_dates.append(ordered_dates_dict.get(date))
+    fin_count = 0
+    unfin_count = 0
+    for color in finished_routes.get(date):
+        fin_count += finished_routes[date][color]
+    for color in unfinished_routes.get(date):
+        unfin_count += unfinished_routes[date][color]
+    total_finished_ratio.append(100 * (fin_count / (fin_count + unfin_count)))
+    total_unfinished_ratio.append(100 * (unfin_count / (unfin_count + fin_count)))
 
-fin_perc = relative_frequency_plot.bar(route_dates_ordered, total_finished_ratio, 0.8, color='green', label='Finished')
-fail_perc = relative_frequency_plot.bar(route_dates_ordered, total_unfinished_ratio, 0.8, bottom=total_finished_ratio, color='orange', label='Failed')
+fin_perc = relative_frequency_plot.bar(total_ratio_dates, total_finished_ratio, 0.8, color='green', label='Finished')
+fail_perc = relative_frequency_plot.bar(total_ratio_dates, total_unfinished_ratio, 0.8, bottom=total_finished_ratio, color='orange', label='Failed')
 relative_frequency_plot.set_title('Percentage Success vs Failure for Bouldering Attempts by Day')
-other_handles = []
-other_handles.append(fin_perc)
-other_handles.append(fail_perc)
+other_handles += [fin_perc, fail_perc]
 
 # plot all legends in single row
 
@@ -252,7 +183,6 @@ legend_plot_route_diff.legend(bubble_handles, bubble_labels)
 legend_plot_route_diff.axis('off')
 
 legend_plot_weight_data.set_title('Weight Graph Legend')
-print('Showing weight_handles data: ' + str(weight_handles))
 weight_labels = get_labels_for_handles(weight_handles)
 legend_plot_weight_data.legend(weight_handles, weight_labels)
 legend_plot_weight_data.axis('off')
