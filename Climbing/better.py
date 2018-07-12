83185884c3e47532ba946d10ba14bd0981c53265
etcadinfinitum <lizzy.presland@gmail.com>
2018-07-11 23:06:27 -0700

    Issue #12 - re-implemented bubble plot!
    Continuing to work on implementing graph(s) with new class construct.

--- Climbing/better.py
+++ Climbing/better.py
@@ -14,10 +14,7 @@ class Data:
     def __init__(self):
 
         self.conversion_constants = { "nan": None, "TRUE": True, "FALSE": False }
-        
         self.route_difficulty = { "Gray" : [0, "V0"], "Yellow": [1, "V1"], "Green": [2, "V2"], "Red": [3, "V3"], "Blue": [4, "V4"], "Orange": [5, "V5"], "Purple": [6, "V6"], "Black": [7, "V7"] }
-        
-        # GET DATA, REFORMAT AS NEEDED
         
         # LAMBDAS
         datefunc = lambda x: mpldates.date2num(datetime.date(int(x[6:]), int(x[0:2]), int(x[3:5])))
@@ -32,6 +29,7 @@ class Data:
         # get an np NDarray of unique dates
         self.unique_dates = np.unique(dates)
         
+        # get bubble arryas
         self.bubble_finish = self.get_bubble_arrays(True, dates, completion, activity, quantity_time, route_level)
         self.bubble_failed = self.get_bubble_arrays(False, dates, completion, activity, quantity_time, route_level)
     
@@ -53,15 +51,13 @@ class Data:
             data[dates_to_indices[date]][difficulty_index] += successes
         return data
         ''' 
+        bubble_icon_size_multiplier = 15
         unique_dates_list = self.unique_dates.tolist()
         data = np.zeros([len(unique_dates_list), len(self.route_difficulty)])
-        # data = []
-        #        for i in range(len(unique_dates)):
-        #    data.append([0 for x in range(len(self.route_difficulty))])
         for i in range(len(dates)):
             if activity[i] == 'Bouldering' and completion[i] == did_you_finish:
                 count = data[unique_dates_list.index(dates[i])][self.route_difficulty[route_level[i]][0]]
-                data[unique_dates_list.index(dates[i])][self.route_difficulty[route_level[i]][0]] = count + quantity_time[i]
+                data[unique_dates_list.index(dates[i])][self.route_difficulty[route_level[i]][0]] = count + (quantity_time[i] * bubble_icon_size_multiplier)
         return data
    
 # the function which produces the graphs 
@@ -83,14 +79,19 @@ def make_plots():
     legend_plot_weight_data = pyp.subplot2grid((6, 3), (5, 1))
     legend_plot_other = pyp.subplot2grid((6, 3), (5, 2))
     
-    summary_bubble_plot.set_xlim(0, len(data.unique_dates))
+    summary_bubble_plot.set_xlim(0, len(data.unique_dates) + 1)
     summary_bubble_plot.set_ylim(0, len(data.route_difficulty))
     summary_bubble_plot.set_title('Route Completion vs Failure by Difficulty')
     summary_bubble_plot.set(xlabel='Session No.', ylabel='Route Difficulty')
-    #    summary_bubble_plot.scatter(unfinished_bubbles[0], unfinished_bubbles[3], c=unfinished_bubbles[1], s=unfinished_bubbles[2], alpha=0.5, marker='s')
-    for i in range(len(data.route_difficulty)):
-        pass
-    # summary_bubble_plot.scatter(finished_bubbles[0], finished_bubbles[3], c=finished_bubbles[1], s=finished_bubbles[2], marker='o', edgecolors='black')
     
+    inv_route_difficulty = { value[0]: key for key, value in data.route_difficulty.items() } 
+    for i in range(len(data.route_difficulty)):
+        summary_bubble_plot.scatter(np.array([data.unique_dates.tolist().index(x) + 1 for x in data.unique_dates]), np.array([i + 0.5 for x in data.unique_dates]), s=data.bubble_finish[:,i], c=np.array([inv_route_difficulty[i].lower() for x in data.unique_dates]), marker='o', edgecolors='black')
+        summary_bubble_plot.scatter(np.array([data.unique_dates.tolist().index(x) + 1 for x in data.unique_dates]), np.array([i + 0.5 for x in data.unique_dates]), s=data.bubble_failed[:,i], c=np.array([inv_route_difficulty[i].lower() for x in data.unique_dates]), marker='s', alpha=0.5)
+    
+    #finishing touches
+    
+    pyp.tight_layout()
+    pyp.savefig('climbing-plot-%s.png' % str(datetime.datetime.today()), bbox_inches='tight')
     
 make_plots()
